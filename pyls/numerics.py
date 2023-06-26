Test Data Added. Subtle bug in VA functions, growing numerical error. Row weighting working.

pyls/numerics.py
@@ -1,6 +1,7 @@
 """Numerical methods for solving the linear system."""
 import numpy as np
 from numba import njit
+from numbers import Number
 
 
 def cart(z):
@@ -16,12 +17,42 @@ def cluster(num_points, L, sigma):
     return pole_spacing
 
 
-def function_handle(self, name):
+def split(coefficients):
+    """Split the coefficients for f and g."""
+    num_coeff = len(coefficients) // 4
+    cf = (
+        coefficients[: 2 * num_coeff : 2]
+        + 1j * coefficients[2 * num_coeff :: 2]
+    )
+    cg = (
+        coefficients[1 : 2 * num_coeff + 1 : 2]
+        + 1j * coefficients[2 * num_coeff + 1 :: 2]
+    )
+    return cf, cg
+
+
+def make_function(name, z, coefficients, hessenbergs, poles):
+    """Make a function with the given name."""
+    z = np.array([z]).reshape(-1, 1)
+    basis, basis_deriv = va_evaluate(z, hessenbergs, poles)
+    cf, cg = split(coefficients)
     match name:
+        case "f":
+            return basis @ cf
+        case "g":
+            return basis @ cg
+        case "psi":
+            return np.imag(np.conj(z) * (basis @ cf) + basis @ cg)
         case "uv":
-            return 1
+            return (
+                z * np.conj(basis_deriv @ cf)
+                - basis @ cf
+                + np.conj(basis_deriv @ cg)
+            )
         case "p":
-            return 1
+            return np.real(4 * basis_deriv @ cf)
+        case "omega":
+            return np.imag(-4 * basis_deriv @ cf)
 
 
 # @njit
@@ -35,8 +66,8 @@ def va_orthogonalise(Z, n, poles=None):
     if Z.shape[1] != 1:
         raise ValueError("Z must be a column vector")
     m = len(Z)
-    H = np.zeros((n + 1, n), dtype=complex)
-    Q = np.zeros((m, n + 1), dtype=complex)
+    H = np.zeros((n + 1, n), dtype=np.complex128)
+    Q = np.zeros((m, n + 1), dtype=np.complex128)
     q = np.ones(len(Z)).reshape(m, 1)
     Q[:, 0] = q.reshape(m)
     for k in range(n):
@@ -49,11 +80,12 @@ def va_orthogonalise(Z, n, poles=None):
     hessenbergs = [H]
     if poles is not None:
         for pole_group in poles:
-            Hp = np.zeros((n + 1, n), dtype=complex)
-            Qp = np.zeros((m, n + 1), dtype=complex)
-            qp = np.ones(len(Z)).reshape(m, 1)
+            num_poles = len(pole_group)
+            Hp = np.zeros((num_poles + 1, num_poles), dtype=np.complex128)
+            Qp = np.zeros((m, num_poles + 1), dtype=np.complex128)
+            qp = np.ones(m).reshape(m, 1)
             Qp[:, 0] = qp.reshape(m)
-            for k in range(n):
+            for k in range(num_poles):
                 qp = Qp[:, k].reshape(m, 1) / (Z - pole_group[k])
                 for j in range(k + 1):
                     Hp[j, k] = np.dot(Qp[:, j].conj(), qp)[0] / m
@@ -95,9 +127,18 @@ def va_evaluate(Z, hessenbergs, poles=None):
     """Construct the basis and its derivatives."""
     H = hessenbergs[0]
     n = H.shape[1]
-    m = len(Z)
-    Q = np.zeros((m, n + 1), dtype=complex)
-    D = np.zeros((m, n + 1), dtype=complex)
+    if isinstance(Z, np.ndarray):
+        m = len(Z)
+        if m != Z.shape[0]:
+            raise ValueError("Z must be a column vector")
+        Z = Z.reshape(-1, 1)
+    elif isinstance(Z, Number):
+        m = 1
+        Z = np.array([Z]).reshape(1, 1)
+    else:
+        raise TypeError("Z must be a numpy array or a number")
+    Q = np.zeros((m, n + 1), dtype=np.complex128)
+    D = np.zeros((m, n + 1), dtype=np.complex128)
     Q[:, 0] = np.ones(m)
     for k in range(n):
         hkk = H[k + 1, k]
@@ -120,11 +161,12 @@ def va_evaluate(Z, hessenbergs, poles=None):
         ).reshape(m)
     if poles is not None:
         for i, pole_group in enumerate(poles):
+            num_poles = len(pole_group)
             Hp = hessenbergs[i + 1]
-            Qp = np.zeros((m, n + 1), dtype=complex)
-            Dp = np.zeros((m, n + 1), dtype=complex)
+            Qp = np.zeros((m, num_poles + 1), dtype=np.complex128)
+            Dp = np.zeros((m, num_poles + 1), dtype=np.complex128)
             Qp[:, 0] = np.ones(m)
-            for k in range(n):
+            for k in range(num_poles):
                 hkk = Hp[k + 1, k]
                 Z_pole = 1 / (Z - pole_group[k])
                 Qp[:, k + 1] = (

pyls/solver.py
@@ -1,8 +1,8 @@
-from pyls.numerics import va_orthogonalise, va_evaluate
+from pyls.numerics import va_orthogonalise, va_evaluate, make_function
 from collections.abc import Sequence
 import scipy.linalg as linalg
 import numpy as np
-from scipy.sparse import csr_matrix
+from scipy.sparse import diags
 import re
 import pickle as pkl
 
@@ -182,9 +182,9 @@ class Solver:
             self.boundary_points, self.hessenbergs, self.domain.poles
         )
         self.construct_linear_system()
-        results = linalg.lstsq(self.A, self.b)
-        self.coefficients = results[0]
-        self.residuals = results[1]
+        self.results = linalg.lstsq(self.A, self.b)
+        self.coefficients = self.results[0]
+        self.residuals = self.results[1]
         self.functions = self.construct_functions()
         if pickle:
             self.pickle_solution(filename)
@@ -205,23 +205,22 @@ class Solver:
         self.apply_boundary_conditions()
         self.A = np.vstack((self.A1, self.A2))
         self.b = np.vstack((self.b1.reshape(m, 1), self.b2.reshape(m, 1)))
-
-    def constuct_functions(self):
-        """Construct the functions from the coefficients."""
+        self.weight_rows()
+    
+    def weight_rows(self):
+        # weight the rows by the distance to the nearest corner
         m = len(self.boundary_points)
-        f_real = self.coefficients[: m + 1]
-        f_imag = self.coefficients[m + 1 : 2 * m + 1]
-        g_real = self.coefficients[2 * m + 1 : 3 * m + 1]
-        g_imag = self.coefficients[3 * m + 1 :]
-        f_coefficients = f_real + 1j * f_imag
-        g_coefficients = g_real + 1j * g_imag
-        f = self.basis @ f_coefficients
-        g = self.basis @ g_coefficients
-        psi = np.imag(np.conj(self.boundary_points) * (f + 1j * g) + g)
-        uv = self.boundary_points * np.conj(f) - f + np.conj(g)
-        p = np.real(4 * np.conj(self.boundary_points) * f)
-        omega = np.imag(-4 * np.conj(self.boundary_points) * f)
-        return [f, g, psi, uv, p, omega]
+        row_weights = np.min(
+            np.abs(
+                self.boundary_points.reshape(-1)[:, np.newaxis]
+                - self.domain.corners[np.newaxis, :]
+            ),
+            axis=1,
+        ).reshape(m, 1)
+        row_weights = np.vstack([row_weights, row_weights])
+        sparse_row_weights = diags(row_weights.reshape(-1))
+        self.A = sparse_row_weights @ self.A
+        self.b = sparse_row_weights @ self.b
 
     def get_dependents(self):
         """Create the dependent variable arrays.
@@ -269,5 +268,33 @@ class Solver:
         with open(filename, "wb") as f:
             pkl.dump(self.functions, f)
 
-    def construct_functions(self, basis_functions):
-        pass
+    def construct_functions(self):
+        def psi(z):
+            return make_function(
+                "psi",
+                z,
+                self.coefficients,
+                self.hessenbergs,
+                self.domain.poles,
+            )
+
+        def uv(z):
+            return make_function(
+                "uv", z, self.coefficients, self.hessenbergs, self.domain.poles
+            )
+
+        def p(z):
+            return make_function(
+                "p", z, self.coefficients, self.hessenbergs, self.domain.poles
+            )
+
+        def omega(z):
+            return make_function(
+                "omega",
+                z,
+                self.coefficients,
+                self.hessenbergs,
+                self.domain.poles,
+            )
+
+        return psi, uv, p, omega

tests/test_boundary_conditions.py
@@ -71,7 +71,7 @@ def test_evaluate_expression():
     sol.validate(expression)
     result = sol.evaluate(expression, 1j * np.linspace(0, 99, 100))
     y = np.linspace(0, 99, 100)
-    expected = sol.U[dom.indicies["0"]] - (y * (1 - y))[:, np.newaxis]
+    expected = sol.U[dom.indices["0"]] - (y * (1 - y))[:, np.newaxis]
     assert np.allclose(result, expected)
 
 

tests/test_va_evaluate.py
@@ -50,15 +50,57 @@ def test_large_va_evaluate():
     assert np.allclose(basis_deriv.imag, basis_deriv_answer.imag)
 
 
-def test_poles_va_evaluate():
+def test_small_poles_va_evaluate_1():
+    from pyls.numerics import va_orthogonalise, va_evaluate
+    import numpy as np
+
+    Z = np.array([-4, 4]).reshape(2, 1)
+    poles = [np.array([0])]
+    hessenbergs, Q = va_orthogonalise(Z, 1, poles)
+    basis, basis_deriv = va_evaluate(Z, hessenbergs, poles)
+    basis_answer = np.array([[1, -1, -1], [1, 1, 1]])
+    basis_deriv_answer = np.array([[0, 1 / 4, -1 / 4], [0, 1 / 4, -1 / 4]])
+    assert np.allclose(basis.real, basis_answer.real)
+    assert np.allclose(basis.imag, basis_answer.imag)
+    assert np.allclose(basis_deriv.real, basis_deriv_answer.real)
+    assert np.allclose(basis_deriv.imag, basis_deriv_answer.imag)
+
+
+def test_small_poles_va_evaluate_2():
+    from pyls.numerics import va_orthogonalise, va_evaluate
+    import numpy as np
+    from scipy.io import loadmat
+
+    Z = loadmat("tests/data/small_poles.mat")["Z"].reshape(3, 1)
+    poles = loadmat("tests/data/small_poles.mat")["Pol"]
+    poles = [
+        poles[i, 0].reshape(
+            -1,
+        )
+        for i in range(poles.shape[0])
+    ]
+    basis_answer = loadmat("tests/data/small_poles.mat")["R0"]
+    basis_deriv_answer = loadmat("tests/data/small_poles.mat")["R1"]
+    hessenbergs, Q = va_orthogonalise(Z, 0, poles)
+    basis, basis_deriv = va_evaluate(Z, hessenbergs, poles)
+    assert np.allclose(basis.real, basis_answer.real)
+    assert np.allclose(basis.imag, basis_answer.imag)
+    assert np.allclose(basis_deriv.real, basis_deriv_answer.real)
+    assert np.allclose(basis_deriv.imag, basis_deriv_answer.imag)
+
+
+def test_large_poles_va_evaluate():
     """Test the va_orthogonalise with poles against the MATLAB code."""
     from pyls.numerics import va_orthogonalise, va_evaluate
     from pyls import Domain
     import numpy as np
     from scipy.io import loadmat
 
-    Z_answer = loadmat("tests/data/lid_driven_cavity_Z.mat")["Z"]
-    poles_answer = loadmat("tests/data/lid_driven_cavity_Pol.mat")["Pol"]
+    test_answers = loadmat("tests/data/lid_driven_cavity.mat")
+    Z_answer = test_answers["Z"]
+    poles_answer = test_answers["Pol"]
+    basis_answer = test_answers["R0"]
+    basis_deriv_answer = test_answers["R1"]
     poles_answer = np.array([poles_answer[0, i] for i in range(4)]).reshape(
         4, 24
     )
@@ -72,10 +114,56 @@ def test_poles_va_evaluate():
     assert np.allclose(dom.boundary_points, Z_answer)
     assert np.allclose(dom.poles, poles_answer)
 
-    basis_answer = loadmat("tests/data/lid_driven_cavity_R0.mat")["R0"]
-    basis_deriv_answer = loadmat("tests/data/lid_driven_cavity_R1.mat")["R1"]
     hessenbergs, Q = va_orthogonalise(
         dom.boundary_points.reshape(1200, 1), 24, poles=dom.poles
+    )
+    basis, basis_deriv = va_evaluate(
+        dom.boundary_points, hessenbergs, poles=dom.poles
+    )
+    # check the polynomial basis is the same
+    assert np.allclose(basis[:, :25], basis_answer[:, :25])
+    assert np.allclose(basis_deriv[:, :25], basis_deriv_answer[:, :25])
+    # check all the basis functions are the same
+    assert np.allclose(basis.real, basis_answer.real)
+    assert np.allclose(basis.imag, basis_answer.imag)
+    assert np.allclose(basis_deriv.real, basis_deriv_answer.real)
+    assert np.allclose(basis_deriv.imag, basis_deriv_answer.imag)
+
+
+def test_large_poles_va_evaluate_hypothesis():
+    """Test the va_orthogonalise with poles against the MATLAB code."""
+    from pyls.numerics import va_orthogonalise, va_evaluate
+    from pyls import Domain
+    import numpy as np
+    from scipy.io import loadmat
+
+    n = 0
+    num_poles = 10
+    test_answers = loadmat(
+        f"tests/data/lid_driven_cavity_n_{n}_np_{num_poles}.mat"
+    )
+    Z_answer = test_answers["Z"]
+    poles_answer = test_answers["Pol"]
+    basis_answer = test_answers["R0"]
+    basis_deriv_answer = test_answers["R1"]
+    poles_answer = np.array([poles_answer[0, i] for i in range(4)]).reshape(
+        4, num_poles
+    )
+    corners = [1 + 1j, -1 + 1j, -1 - 1j, 1 - 1j]
+    dom = Domain(
+        corners,
+        num_poles=num_poles,
+        num_boundary_points=300,
+        L=np.sqrt(2) * 1.5,
+    )
+    # check the MATALB domain points and poles are the same
+    # check the polynomial coefficients are the same
+    assert np.allclose(dom.boundary_points, Z_answer)
+    assert np.allclose(dom.boundary_points, Z_answer)
+    assert np.allclose(dom.poles, poles_answer)
+
+    hessenbergs, Q = va_orthogonalise(
+        dom.boundary_points.reshape(1200, 1), 0, poles=dom.poles
     )
     basis, basis_deriv = va_evaluate(
         dom.boundary_points, hessenbergs, poles=dom.poles
@@ -94,4 +182,7 @@ if __name__ == "__main__":
     test_import_va_evaluate()
     test_small_va_evaluate()
     test_large_va_evaluate()
-    test_poles_va_evaluate()
+    test_small_poles_va_evaluate_1()
+    test_small_poles_va_evaluate_2()
+    test_large_poles_va_evaluate()
+    # test_large_poles_va_evaluate_hypothesis()

tests/test_va_orthogonalise.py
@@ -76,11 +76,15 @@ def test_poles_va_orhtogonalise():
     import numpy as np
     from scipy.io import loadmat
 
-    H_answer = loadmat("tests/data/lid_driven_cavity_Hes.mat")["Hes"]
+    n, num_poles = 24, 24
+    test_answers = loadmat(
+        f"tests/data/lid_driven_cavity_n_{n}_np_{num_poles}.mat"
+    )
+    H_answer = test_answers["Hes"]
     hessenbergs_answer = [H_answer[:, k][0] for k in range(H_answer.shape[1])]
-    Q_answer = loadmat("tests/data/lid_driven_cavity_Q.mat")["Q"]
-    Z_answer = loadmat("tests/data/lid_driven_cavity_Z.mat")["Z"]
-    poles_answer = loadmat("tests/data/lid_driven_cavity_Pol.mat")["Pol"]
+    Q_answer = test_answers["Q"]
+    Z_answer = test_answers["Z"]
+    poles_answer = test_answers["Pol"]
     poles_answer = np.array([poles_answer[0, i] for i in range(4)]).reshape(
         4, 24
     )