f3e7e8b95724293071970534d77e33ffc7232929
Ben Mindlin <bennm37@gmail.com>
2023-08-04 09:18:31 +0100

    index on periodic: 833e713 Doubly Periodic Simulation (sort of) Working

--- pylars/__init__.py
+++ pylars/__init__.py
@@ -1,6 +1,7 @@
 """Solve stokes flow problems using the lightning stokes method."""
 from .numerics import va_orthogonalise  # noqa: F401
 from .domain import Domain  # noqa: F401
+from .periodic_domain import PeriodicDomain  # noqa: F401
 from .problem import Problem  # noqa: F401
 from .solver import Solver  # noqa: F401
 from .solution import Solution  # noqa: F401
