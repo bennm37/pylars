a1551a2d8fe3149ba484bda26fa47736b8ceffda
Ben Mindlin <bennm37@gmail.com>
2023-08-25 22:58:09 +0100

    COMSOL cylinder stress script

--- scripts/COMSOL_cylinder_stress_0.2.py
+++ scripts/COMSOL_cylinder_stress_0.2.py
@@ -75,84 +75,107 @@ def get_pylars_solution(center=0.2j):
     return sol
 
 
-center = 0.2j
-data = load_comsol_data(center=center.imag)
-sol = get_pylars_solution(center=center)
-an = Analysis(sol)
-fig, ax = an.plot(
-    resolution=201,
-    interior_patch=True,
-    enlarge_patch=1.02,
-    streamline_type="starting_points",
-    n_streamlines=20,
-)
-plt.show()
-
-# calculate traction data
-mesh_sizes = data.keys()
-circle = lambda t: center + 0.5 * np.exp(t * 1j)
-normal = lambda t: -np.exp(t * 1j)
-pylars_data = {}
-# mesh_sizes = ["extremely_fine"]
-for size in mesh_sizes:
-    theta = data[size][0][:, 0]
-    z = circle(np.array(theta))
-    stress_tensor = sol.stress_goursat(z)
-    normals = normal(np.array(theta))
-    normals = np.array([normals.real, normals.imag]).T
-    surface_stress = np.array([S @ n for n, S in zip(normals, stress_tensor)])
-    pylars_data[size] = surface_stress
-
-
-for size in mesh_sizes:
-    fig, ax = plt.subplots(1, 2)
-    sample = 1
-    theta = np.array(data[size][0][:, 0])
-    # theta = np.linspace(0, 2 * np.pi, 100)
-    stress_x = np.array(data[size][0][:, 1])
-    stress_y = np.array(data[size][1][:, 1])
-    pylars_stress_x = pylars_data[size][:, 0]
-    pylars_stress_y = pylars_data[size][:, 1]
-    ax[0].plot(theta[::sample], stress_x[::sample], label="COMSOL Stress X")
-    ax[0].plot(theta[::sample], stress_y[::sample], label="COMSOL Stress Y")
-    ax[0].plot(
-        theta,
-        pylars_stress_x,
-        c="k",
-        label="PyLARS Stress X",
+if __name__ == "__main__":
+    center = 0.2j
+    data = load_comsol_data(center=center.imag)
+    sol = get_pylars_solution(center=center)
+    an = Analysis(sol)
+    fig, ax = an.plot(
+        resolution=201,
+        interior_patch=True,
+        enlarge_patch=1.02,
+        streamline_type="starting_points",
+        n_streamlines=20,
     )
-    ax[0].plot(
-        theta,
-        pylars_stress_y,
-        linestyle="--",
-        c="darkgreen",
-        label="PyLARS Stress Y",
-    )
-    ax[0].set(
-        xlabel=" $\Theta$ ",
-        ylabel="Traction",
-        title="Traction on Cylinder",
-    )
-    ax[0].legend()
-    sample = 10
-    ax[1].plot(
-        theta[::sample],
-        stress_x[::sample] - pylars_stress_x[::sample],
-        label="Error X",
-    )
-    ax[1].plot(
-        theta[::sample],
-        stress_y[::sample] - pylars_stress_y[::sample],
-        label="Error Y",
-    )
-    ax[1].set(
-        xlabel=" $\Theta$ ",
-        ylabel="Error",
-        title="Error in Traction on Cylinder",
-    )
-    ax[1].legend()
-    # plt.savefig("media/COMSOL_vs_PyLARS_stress.pdf")
-    plt.tight_layout()
     plt.show()
 
-    fig, ax = plt.subplots(1, 2)
+    # calculate traction data
+    mesh_sizes = data.keys()
+    circle = lambda t: center + 0.5 * np.exp(t * 1j)
+    normal = lambda t: -np.exp(t * 1j)
+    pylars_data = {}
+    # mesh_sizes = ["extremely_fine"]
+    for size in mesh_sizes:
+        theta = data[size][0][:, 0]
+        z = circle(np.array(theta))
+        stress_tensor = sol.stress_goursat(z)
+        normals = normal(np.array(theta))
+        normals = np.array([normals.real, normals.imag]).T
+        surface_stress = np.array(
+            [S @ n for n, S in zip(normals, stress_tensor)]
+        )
+        pylars_data[size] = surface_stress
+
+    for size in mesh_sizes:
+        fig, ax = plt.subplots(1, 2)
+        sample = 1
+        theta = np.array(data[size][0][:, 0])
+        # theta = np.linspace(0, 2 * np.pi, 100)
+        stress_x = np.array(data[size][0][:, 1])
+        stress_y = np.array(data[size][1][:, 1])
+        pylars_stress_x = pylars_data[size][:, 0]
+        pylars_stress_y = pylars_data[size][:, 1]
+        ax[0].plot(
+            theta[::sample], stress_x[::sample], label="COMSOL Stress X"
+        )
+        ax[0].plot(
+            theta[::sample], stress_y[::sample], label="COMSOL Stress Y"
+        )
+        ax[0].plot(
+            theta,
+            pylars_stress_x,
+            c="k",
+            label="PyLARS Stress X",
+        )
+        ax[0].plot(
+            theta,
+            pylars_stress_y,
+            linestyle="--",
+            c="darkgreen",
+            label="PyLARS Stress Y",
+        )
+        ax[0].set(
+            xlabel=" $\Theta$ ",
+            ylabel="Traction",
+            title="Traction on Cylinder",
+        )
+        ax[0].legend()
+        sample = 10
+        ax[1].plot(
+            theta[::sample],
+            stress_x[::sample] - pylars_stress_x[::sample],
+            label="Error X",
+        )
+        ax[1].plot(
+            theta[::sample],
+            stress_y[::sample] - pylars_stress_y[::sample],
+            label="Error Y",
+        )
+        ax[1].set(
+            xlabel=" $\Theta$ ",
+            ylabel="Error",
+            title="Error in Traction on Cylinder",
+        )
+        ax[1].legend()
+        # plt.savefig("media/COMSOL_vs_PyLARS_stress.pdf")
+        plt.tight_layout()
+        plt.show()
+
+    # plot convergence of the max error
+    max_errors_x = [
+        np.max(np.abs(np.array(data[size][0][:, 1]) - pylars_data[size][:, 0]))
+        for size in mesh_sizes
+    ]
+    max_errors_y = [
+        np.max(np.abs(np.array(data[size][1][:, 1]) - pylars_data[size][:, 1]))
+        for size in mesh_sizes
+    ]
+    fig, ax = plt.subplots()
+    ax.plot(max_errors_x, label="Max Error X")
+    ax.plot(max_errors_y, label="Max Error Y")
+    ax.set_xlabel("Mesh Size")
+    ax.set_ylabel("Max Error")
+    ax.set_xticks(range(len(mesh_sizes)))
+    ax.set_xticklabels(mesh_sizes)
+    ax.legend()
+    plt.show()
